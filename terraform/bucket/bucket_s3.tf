fff3f55bd610db658b012ee71a3cd90261679a08
Maksim Matyushkin <matyshkinm@evrone.com>
2023-10-24 14:19:30 +0300

    bukcet

--- terraform/bucket/bucket_s3.tf
+++ terraform/bucket/bucket_s3.tf
@@ -1,23 +1,34 @@
-resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
-  service_account_id = var.service_account_id
-  description        = "static access key for object storage"
-}
+# resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+#   service_account_id = var.service_account_id
+#   description        = "static access key for object storage"
+# }
 
-// Use keys to create bucket
-resource "yandex_storage_bucket" "this" {
-  for_each = { for bucket in var.buckets : bucket.name => bucket }
+# // Use keys to create bucket
+# resource "yandex_storage_bucket" "this" {
+#   for_each = { for bucket in var.buckets : bucket.name => bucket }
 
-  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-  # folder_id  = var.folder_id
+#   access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+#   secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+#   # folder_id  = var.folder_id
 
-  bucket = each.key
-  acl    = each.value.acl
-}
+#   bucket = each.key
+#   acl    = each.value.acl
+# }
 
 
-resource "yandex_storage_bucket" "bucket" {
-  count  = length(var.buckets)
-  bucket = var.buckets[count.index].name
-  acl    = var.buckets[count.index].acl
+# resource "yandex_storage_bucket" "bucket" {
+#   count  = length(var.buckets)
+#   bucket = var.buckets[count.index].name
+#   acl    = var.buckets[count.index].acl
+# }
+
+
+
+resource "yandex_storage_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
 }
