38bc0ae148b7f9d3527bc23b869eae70a7a1cf05
Maksim Matyushkin <matyshkinm@evrone.com>
2023-10-24 17:05:28 +0300

    bukcet

--- terraform/bucket/bucket_s3.tf
+++ terraform/bucket/bucket_s3.tf
@@ -15,8 +15,8 @@ resource "yandex_storage_bucket" "this" {
   acl    = each.value.acl
 }
 
-resource "yandex_storage_bucket" "bucket" {
-  count  = length(var.buckets)
-  bucket = var.buckets[count.index].name
-  acl    = var.buckets[count.index].acl
-}
+# resource "yandex_storage_bucket" "bucket" {
+#   count  = length(var.buckets)
+#   bucket = var.buckets[count.index].name
+#   acl    = var.buckets[count.index].acl
+# }
