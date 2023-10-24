40e726142110c4f5560ece9a7e2d0a81f2e51151
Maksim Matyushkin <matyshkinm@evrone.com>
2023-10-24 17:51:35 +0300

    +varibles infra

--- terraform/infra/variables.tf
+++ terraform/infra/variables.tf
@@ -17,3 +17,33 @@ variable "github_url" {
   type    = string
   default = "ssh://git@github.com/matyushkim/test_yc_argo.git"
 }
+
+variable "service_account_id" {
+  type        = string
+  description = "Service account ID"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "Folder ID"
+  sensitive   = true
+
+}
+
+variable "token" {
+  type        = string
+  description = "token"
+  sensitive   = true
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "cloud_id"
+  sensitive   = true
+
+}
+
+variable "zone" {
+  type        = string
+  description = "zone"
+}
