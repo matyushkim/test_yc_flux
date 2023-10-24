4d5200c884b9edfdbf8e3074ba74624465b8014f
Maksim Matyushkin <matyshkinm@evrone.com>
2023-10-24 16:27:09 +0300

    bukcet

--- terraform/bucket/provider.tf
+++ terraform/bucket/provider.tf
@@ -1,16 +1,15 @@
+provider "yandex" {
+  token     = var.token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
+}
 terraform {
-  # required_version = ">=1.1.5"
+  required_version = ">=1.1.5"
 
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
     }
   }
-  required_version = ">= 0.13"
-}
-provider "yandex" {
-  token     = var.token
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
-  zone      = var.zone
 }
