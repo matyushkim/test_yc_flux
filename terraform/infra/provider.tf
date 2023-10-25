1c3518c1910d4f3b855d2f26543a605e55d86b5e
Maksim Matyushkin <matyshkinm@evrone.com>
2023-10-25 11:27:53 +0300

    changed source

--- terraform/infra/provider.tf
+++ terraform/infra/provider.tf
@@ -8,18 +8,18 @@ provider "yandex" {
 terraform {
   required_version = ">=1.1.5"
 
-  backend "s3" {
-    bucket   = "evronelabs-webapplication-store"
-    key      = "infra/terraform.tfstate"
-    endpoint = "storage.yandexcloud.net"
-    region   = var.zone
-    # dynamodb_table = "aws-terraform-states-lock"
-    encrypt = true
-    # terraform init \
-    # -backend-config="bucket=evronelabs-webapplication-store" \
-    # -backend-config="endpoint=storage.yandexcloud.net" \
-    # -backend-config="region= ${{ vars.YC_REGION }} "
-  }
+  # backend "s3" {
+  #   bucket   = "evronelabs-webapplication-store"
+  #   key      = "infra/terraform.tfstate"
+  #   endpoint = "storage.yandexcloud.net"
+  #   region   = var.zone
+  #   # dynamodb_table = "aws-terraform-states-lock"
+  #   encrypt = true
+  #   # terraform init \
+  #   # -backend-config="bucket=evronelabs-webapplication-store" \
+  #   # -backend-config="endpoint=storage.yandexcloud.net" \
+  #   # -backend-config="region= ${{ vars.YC_REGION }} "
+  # }
 
   required_providers {
     flux = {
