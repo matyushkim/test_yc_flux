9647a7708fce89997ba2795d002fc6b91a0eb31a
Maksim Matyushkin <matyshkinm@evrone.com>
2023-10-25 13:54:18 +0300

    infra

--- terraform/bucket/bucket_s3.tf
+++ terraform/bucket/bucket_s3.tf
@@ -1,15 +1,15 @@
-resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
-  service_account_id = var.service_account_id
-  description        = "static access key for object storage"
-}
+# resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+#   service_account_id = var.service_account_id
+#   description        = "static access key for object storage"
+# }
 
 // вариант создания через for each с ключами
 resource "yandex_storage_bucket" "this" {
   for_each = { for bucket in var.buckets : bucket.name => bucket }
 
-  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-  folder_id  = var.folder_id
+  # access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  # secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  # folder_id  = var.folder_id
 
   bucket = each.key
   acl    = each.value.acl
