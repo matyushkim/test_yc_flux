8769864a2c8825b1920482f1bdcae421619c3d93
Maksim Matyushkin <matyshkinm@evrone.com>
2023-10-25 12:57:14 +0300

    infra

--- terraform/infra/kube.tf
+++ terraform/infra/kube.tf
@@ -1,5 +1,5 @@
 module "kube" {
-  source     = "git@github.com:terraform-yc-modules/terraform-yc-kubernetes.git"
+  source     = "https://github.com/terraform-yc-modules/terraform-yc-kubernetes.git"
   network_id = module.yc-vpc.vpc_id
 
   master_locations = [
