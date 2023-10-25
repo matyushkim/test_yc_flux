0475072e5ac84be792bd0a932bf717a768a8e78c
Maksim Matyushkin <matyshkinm@evrone.com>
2023-10-25 13:36:04 +0300

    infra

--- terraform/infra/provider.tf
+++ terraform/infra/provider.tf
@@ -9,15 +9,28 @@ terraform {
   required_version = ">=1.1.5"
 
   backend "s3" {
-    endpoint = "storage.yandexcloud.net"
+    # endpoint = "storage.yandexcloud.net"
+    # bucket   = "evronelabs-terraform-state-store"
+    # region   = "ru-central1"
+    # key      = "infra/terraform.tfstate"
+    # # access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+    # # secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+
+    # skip_credentials_validation = true
+    # skip_metadata_api_check     = true
+
+
     bucket   = "evronelabs-terraform-state-store"
-    region   = "ru-central1"
     key      = "infra/terraform.tfstate"
-    # access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-    # secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+    region   = "ru-central1"
+    endpoint = "https://storage.yandexcloud.net"
+    encrypt  = false
 
     skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_bucket_ssencryption    = true
     skip_metadata_api_check     = true
+    skip_bucket_root_access     = true
   }
 
   required_providers {
