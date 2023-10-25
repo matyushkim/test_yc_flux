e7f378d184b06f645bf360a5c74e585bc128a84f
Maksim Matyushkin <matyshkinm@evrone.com>
2023-10-25 13:55:56 +0300

    infra

--- terraform/bucket/bucket_s3.tf
+++ terraform/bucket/bucket_s3.tf
@@ -9,7 +9,7 @@ resource "yandex_storage_bucket" "this" {
 
   # access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
   # secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-  # folder_id  = var.folder_id
+  folder_id = var.folder_id
 
   bucket = each.key
   acl    = each.value.acl
