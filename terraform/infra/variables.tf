6286dbea95af3b897602ad0e04b0bc03289f8ae7
Maksim Matyushkin <matyshkinm@evrone.com>
2023-10-25 14:48:59 +0300

    infra

--- terraform/infra/variables.tf
+++ terraform/infra/variables.tf
@@ -75,33 +75,3 @@ variable "buckets" {
   ]
   description = "List of bucket names with acls"
 }
-
-variable "service_account_id" {
-  type        = string
-  description = "Service account ID"
-}
-
-variable "folder_id" {
-  type        = string
-  description = "Folder ID"
-  sensitive   = true
-
-}
-
-variable "token" {
-  type        = string
-  description = "token"
-  sensitive   = true
-}
-
-variable "cloud_id" {
-  type        = string
-  description = "cloud_id"
-  sensitive   = true
-
-}
-
-variable "zone" {
-  type        = string
-  description = "zone"
-}
