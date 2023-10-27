666a50f6b5b421bdde7ef45c26ddfa6477584c23
Maksim Matyushkin <matyshkinm@evrone.com>
2023-10-27 10:38:14 +0300

    flux new key

--- terraform/infra/fluxcd.tf
+++ terraform/infra/fluxcd.tf
@@ -29,7 +29,7 @@ resource "tls_private_key" "flux" {
 }
 
 resource "github_repository_deploy_key" "this" {
-  title      = "Flux"
+  title      = "Flux_new"
   repository = var.github_repository
   key        = tls_private_key.flux.public_key_openssh
   read_only  = "false"
