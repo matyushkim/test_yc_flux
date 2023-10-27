0a581f48b734d8f0d86dd5b499c6c7db8e1af116
Maksim Matyushkin <matyshkinm@evrone.com>
2023-10-27 14:06:32 +0300

    seatins flux

--- terraform/infra/fluxcd.tf
+++ terraform/infra/fluxcd.tf
@@ -29,12 +29,11 @@ resource "tls_private_key" "flux" {
 }
 
 resource "github_repository_deploy_key" "this" {
-  title      = "Flux"
+  title      = "Flux_new_iam"
   repository = var.github_repository
   key        = tls_private_key.flux.public_key_openssh
   read_only  = "false"
 }
-
 
 resource "flux_bootstrap_git" "this" {
   depends_on             = [github_repository_deploy_key.this]
