151805852a5b487b678b77fe0ba96b7fa4c84730
Maksim Matyushkin <matyshkinm@evrone.com>
2023-10-30 13:10:59 +0300

    seatins loki

---
+++ terraform/infra/secrets.tf
@@ -0,0 +1,12 @@
+resource "kubernetes_secret" "aws_credentials" {
+  metadata {
+    name      = "aws-credentials"
+    namespace = loki
+  }
+
+  data = {
+    AWS_ACCESS_KEY_ID     = var.aws_access_key_id
+    AWS_SECRET_ACCESS_KEY = var.aws_secret_access_key
+  }
+
+}
