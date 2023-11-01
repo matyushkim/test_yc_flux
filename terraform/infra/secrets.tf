23cd9bae79e8ce814fb885083f29fba5cb0bb79c
Maksim Matyushkin <matyshkinm@evrone.com>
2023-11-01 10:54:46 +0300

    mimir

--- terraform/infra/secrets.tf
+++ terraform/infra/secrets.tf
@@ -14,3 +14,14 @@ resource "kubernetes_secret" "aws_credentials" {
     AWS_SECRET_ACCESS_KEY = var.aws_secret_access_key
   }
 }
+
+resource "kubernetes_secret" "mimir_credentials" {
+  metadata {
+    name      = "aws-credentials"
+    namespace = "mimir"
+  }
+  data = {
+    AWS_ACCESS_KEY_ID     = var.aws_access_key_id
+    AWS_SECRET_ACCESS_KEY = var.aws_secret_access_key
+  }
+}
