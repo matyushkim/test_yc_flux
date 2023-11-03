5d4f8f3963f96718b0dcf3031aeb8d8886b81f9c
Maksim Matyushkin <matyshkinm@evrone.com>
2023-11-03 18:35:58 +0300

    NEW-4

--- terraform/infra/kube.tf
+++ terraform/infra/kube.tf
@@ -22,18 +22,19 @@ module "kube" {
 
   node_groups = {
     "yc-k8s-dev" = {
-      description   = "Kubernetes nodes group development"
-      platform_id   = "standard-v3"
-      node_cores    = 4
-      node_memory   = 8
-      node_gpus     = 0
-      core_fraction = 20
-      disk_type     = "network-hdd"
-      disk_size     = 30
-      preemptible   = true
-      nat           = false
-      ipv4          = true
-      ipv6          = false
+      description       = "Kubernetes nodes group development"
+      platform_id       = "standard-v3"
+      node_cores        = 4
+      node_memory       = 8
+      node_gpus         = 0
+      core_fraction     = 20
+      disk_type         = "network-hdd"
+      disk_size         = 30
+      node_gpus_version = var.cluster_version
+      preemptible       = true
+      nat               = false
+      ipv4              = true
+      ipv6              = false
       fixed_scale = {
         size = 1
       }
