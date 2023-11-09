82a691bec71110edabcb0760bb00216345603104
Maksim Matyushkin <matyshkinm@evrone.com>
2023-11-09 14:13:26 +0300

    SecretSync and new kubsecret

--- terraform/infra/secrets.tf
+++ terraform/infra/secrets.tf
@@ -27,7 +27,6 @@ resource "kubernetes_secret" "replicator" {
   depends_on = [kubernetes_namespace.secret]
 }
 
-
 # resource "kubernetes_secret" "loki-aws" {
 #   metadata {
 #     name      = "loki-aws"
